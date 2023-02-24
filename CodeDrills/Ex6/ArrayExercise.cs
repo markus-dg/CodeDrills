41e60d745dfb88711c0f09769c1871bb3c9efbdd
Markus Mueller <markus.mueller@digitecgalaxus.ch>
2023-02-24 15:21:02 +0100

    Clearify comments

--- CodeDrills/Ex6/ArrayExercise.cs
+++ CodeDrills/Ex6/ArrayExercise.cs
@@ -2,7 +2,6 @@ namespace CodeDrills.Ex6;
 
 public class ArrayExercise
 {
-   
     public int GetThirdElement(int[] xs)
     {
         // return the third element of the given array. Assume that the array has at least 3 elements.
@@ -11,46 +10,46 @@ public class ArrayExercise
 
     public int GetIthElement(int[] xs, int i)
     {
-        // return the i-th element of the given array. Assume that the array has at least i elements.
+        // return the i-th element of the given array. Assume that the array has at least `i` elements.
         return 0;
     }
 
     public int SumOfAllElements(int[] xs)
     {
         // Use a loop to iterate over the array and sum up all elements.
-        
+
         return 0;
     }
 
     public int[] SetSecondElementTo5(int[] xs)
     {
-
+        // Assume that the array has at least 2 elements.
         return xs;
     }
 
     public int[] SetIthElementTo3(int[] xs, int i)
     {
+        // Assume that the array has at least `i` elements.
         return xs;
     }
 
     public int[] SetIthElementToValue(int[] xs, int i, int value)
     {
-        
+        // Assume that the array has at least `i` elements.
         return xs;
     }
 
     public int[] SetAllElementsTo7(int[] xs)
     {
-        
         // Use a loop to iterate over the array and set all elements to 7.
-        
+
         return xs;
     }
-    
+
     public int FindSmallestElement(int[] xs)
     {
         // Find the smallest element: Use a loop to iterate over the array and find the smallest element.
-        
+
         // return the smallest elements of the array.
         return 0;
     }
@@ -60,7 +59,7 @@ public class ArrayExercise
         // Create a new array to hold the unique elements from the original array.
 
         // Check for duplicates: Use a loop to iterate over the original array and check if each element is already in the new array. If it's not, add it to the new array.
-        
+
         // return the unique elements of the original array.
 
         return Array.Empty<int?>();
